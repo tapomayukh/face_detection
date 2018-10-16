34bb2f03921c19be1acad5cf128067565b567906
Ethan Gordon <ekgordon@cs.washington.edu>
2018-10-16 11:47:44 -0700

    Removed hard-coded path

--- src/MouthPose.cpp
+++ src/MouthPose.cpp
@@ -5,6 +5,7 @@
 #include <dlib/image_processing.h>
 #include <dlib/gui_widgets.h>
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <image_transport/image_transport.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <visualization_msgs/Marker.h>
@@ -449,7 +450,8 @@ int main(int argc, char **argv)
    image_transport::ImageTransport it(nh);
 
    std::string MarkerTopic = "/camera/color/image_raw";
-   deserialize("/home/egordon/Workspace/ada_ws/src/face_detection/model/shape_predictor_68_face_landmarks.dat") >> predictor;
+   std::string path = ros::package::getPath("face_detection");
+   deserialize(path + "/model/shape_predictor_68_face_landmarks.dat") >> predictor;
    ros::Subscriber sub_info = nh.subscribe("/camera/color/camera_info", 1, cameraInfo);
    ros::Subscriber sub_depth = nh.subscribe("/camera/aligned_depth_to_color/image_raw", 1, DepthCallBack );
    image_transport::Subscriber sub = it.subscribe("/camera/color/image_raw", 1, imageCallback, image_transport::TransportHints("compressed"));
